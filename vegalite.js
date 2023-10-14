3fb00a684c3363b45e721af36e28465cde1384ec
ophelianz02 <142366889+ophelianz02@users.noreply.github.com>
2023-10-14 15:15:59 +1100

    Update vegalite.js

--- vegalite.js
+++ vegalite.js
@@ -3,6 +3,6 @@ vegaEmbed("#cloroplethmap", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var vg_2 = "scatterplot.vg.json";
-vegaEmbed("#scatterplot", vg_1).then(function(result) {
+vegaEmbed("#scatterplot", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
