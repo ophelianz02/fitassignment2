1a4cc4c31f3bd1ee054c66a32aa948e178d8c8ed
ophelianz02 <142366889+ophelianz02@users.noreply.github.com>
2023-10-14 17:42:25 +1100

    Update vegalite.js

--- vegalite.js
+++ vegalite.js
@@ -6,3 +6,7 @@ var vg_2 = "scatterplot.vg.json";
 vegaEmbed("#scatterplot", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_3 = "barchart.vg.json";
+vegaEmbed("#barchart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
